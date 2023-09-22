ee45ea84fe4585729808f9dcfc1ad5b4a5a1c95c
Noah A Klem <noahaklem@gmail.com>
2023-09-22 10:14:40 -0600

    orm methods with querying

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,65 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+      student = self.new
+      student.id = row[0]
+      student.name = row[1]
+      student.grade = row[2]
+      student
     # create a new Student object given a row from the database
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, 9).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < ?
+    SQL
+
+    DB[:conn].execute(sql, 12).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
