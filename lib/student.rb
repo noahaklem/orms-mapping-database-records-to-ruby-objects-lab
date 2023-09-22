1f09d7aa7250749868a663ec8a1ccc73c352dcf3
Noah A Klem <noahaklem@gmail.com>
2023-09-22 10:38:25 -0600

    query methods orms

--- lib/student.rb
+++ lib/student.rb
@@ -62,6 +62,44 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_X_students_in_grade_10(limit)
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = ?
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, 10, limit).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, 10).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
   def save
     sql = <<-SQL
